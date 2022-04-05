257be0ba5d1bb4452408c7d710b8ed3918222822
camilomonje <lumonje12@gmail.com>
2022-04-04 19:03:09 -0500

    Se agrega semantic-ui-css y semantic-ui-react

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import React, { Component } from 'react';
 import { _getPhones } from '../utils/_DATA'
 import { receivePhones } from '../actions/phones'
-import Container from './Container'
+import Container from './container'
 
 class App extends Component {
   componentDidMount() {
